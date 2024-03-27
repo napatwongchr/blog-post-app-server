e98c9c35f04b291d840b7dc5d0f04a31e6a2bf37
napatwongchr <napat.wongchr@gmail.com>
2024-03-27 09:52:16 +0700

    feat: add creating data api and logic comment

--- server/app.mjs
+++ server/app.mjs
@@ -7,6 +7,18 @@ app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
 
+app.post("/posts", (req, res) => {
+  // ลอจิกในการเก็บข้อมูลของโพสต์ลงในฐานข้อมูล
+
+  // 1) Access ข้อมูลใน Body จาก Request ด้วย req.body
+  // 2) เขียน Query เพื่อ Insert ข้อมูลโพสต์ ด้วย Connection Pool
+  // 3) Return ตัว Response กลับไปหา Client ว่าสร้างสำเร็จ
+
+  return res.status(201).json({
+    message: "Created post sucessfully",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
